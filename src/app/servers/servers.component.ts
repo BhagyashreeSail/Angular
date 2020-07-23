f1a40bb29099dd52ee675bd3e8e9b4e2a4112fe4
BhagyashreeSail <bhagyashreesail08@gmail.com>
2020-07-23 13:55:38 +0530

    Data Binding

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer=false;
+  serverCreationStatus='No server created';
+  serverName='TestServer';
+
+  constructor() { 
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    },2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus='Server is created. The name is '+ this.serverName;
+
+  }
+
+  onUpdateserverName(event:Event){
+    this.serverName=(<HTMLInputElement>event.target).value;
+  }
+
+}
