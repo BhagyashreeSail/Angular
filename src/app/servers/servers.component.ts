f92a22de136d3b34860aa716131c51104147e9f3
BhagyashreeSail <33345149+BhagyashreeSail@users.noreply.github.com>
2020-07-23 18:30:50 +0530

    Update servers.component.ts

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer=false;
   serverCreationStatus='No server created';
   serverName='TestServer';
+  username:any;
 
   constructor() { 
     setTimeout(()=>{
